865e35703e1a0c70e1edf7f994ea2c57eb992925
alfabettagamak <alisa.skrynko@gmail.com>
2024-06-10 21:05:51 +0300

    arrays

--- AQAjava/src/Main.java
+++ AQAjava/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -5,99 +7,77 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//        String firstStr = "12345";
-//        String firstStr1 = new String("12345");
-//
-//        int a = 5;
-//
-//        String secondFirst = "12345";
-//        String secondStr1 = new String("12345");
-//
-//        Boolean result = firstStr1 == secondStr1;
-//
-//        Boolean result1 = firstStr.equals(secondFirst);
-//        char aa = 'a';
 
-//        System.out.println("Введите ваш возраст");
-        Scanner input = new Scanner(System.in);
-//        int age = input.nextInt();
+
+
+          String str = "21132123-dfsdfsd-23432423-3333-xczcxzc-3311334";
+          int counter = -1;
+          int index = 5555;
+          for (String strItem: str.split("")){
+              counter++;
+              if (strItem.equals("d")){
+                  index = counter;
+                  break;
+              }
+          }
+        System.out.println(index);
+
 //
-//        if (age  < 18) {
-//            System.out.println("Разрешение родителей");
-//            System.out.println("Оно у вас есть?" );
-//            String answer = input.next();
-//            if (answer.equals("ДА") || answer.equals("да") || answer.equals("yes")) {
-//                System.out.println("Пришлите разрешение на почту");
+//        System.out.println(str.substring(str.indexOf('d'), str.indexOf("23432423")));
+//        System.out.println(str.substring(1));
+//
+//
+//        String [] strArray = str.split("-");
+//        boolean isCorrect = false;
+//        for (String item: strArray) {
+//            if (item.matches("[0-9]*")) {
+//                isCorrect = true;
+//                break;
 //            }
 //        }
-//        else if (age < 50 && age > 25){ // || - или
-//            System.out.println("20% ставка");
-//        }
-//        else {
-//            System.out.println("отказ");
-//        }
-//
-//        System.out.println("Введите значение 1, 2, 3 или 4");
-//
-//        int number = input.nextInt();
-//
-//        String str = (number == 100) ? "Сто" : "Не сто";
-//
-//        switch (number){
-//            case 1:
-//                System.out.println("args =  1");
-//                break;
-//            case 2:
-//                System.out.println("args =  2");
-//                break;
-//            default:
-//                System.out.println("Не подходит");
-//        }
-//         final int months = 12;
-//         // ! 0 = 1
-//        // ! 3 = 1 * 2 * 3
-//         int sum = 0;
-//         for (int i = 1; i <= 10; i++){
-//              //sum = sum + i;
-//              sum += i;
-//             if (i != 10) {
-//                 System.out.print(i + " + ");
-//             }
-//             else
-//                 System.out.print(i);
-//         }
-//        System.out.println(" = " + sum);
-        //  1 * 1    2 * 1
-        //  1 * 2    2 * 2
-        //  1 * 3    2 + 3
-        for (int i = 1; i <= 9; i++){
-            for (int j = 1; j <= 9; j++){
-                System.out.print(i);
-                System.out.print(" * ");
-                System.out.print(j);
-                System.out.print(" = ");
-                System.out.print(i * j);
-                System.out.println();
-            }
-            System.out.println();
-        }
+//        System.out.println("is Correct? " + isCorrect);
 
-        Random rnd = new Random();
+//        System.out.println("Age :");
+//        Scanner input = new Scanner(System.in);
+//        int age = input.nextInt();
+//
+//        String[] list = {"Milk", "Beer", "Tomato", "Salt", "Bread", "Water"};
+//        for (String item: list){
+//            if (age < 18 && item.equals("Beer")) continue;
+//            System.out.println(item);
+//        }
 
-        int rndNumber = 0;
-        int k = 0;
-        while (k < 9){
-            // 1000  1000000
-            rndNumber = 1000 + rnd.nextInt(1000000);
-            System.out.println(rndNumber);
-            k++;
-        }
+        int [] arrayNumbers = new int[100];
 
-        do {
-            rndNumber = rnd.nextInt(10000);
-            System.out.println(rndNumber);
-            k++;
-        } while (k < 15);
+//        Random rnd = new Random();
+//        for(int i = 0; i < arrayNumbers.length; i++){
+//            int number = rnd.nextInt(100);
+//            arrayNumbers[i] = number;
+//            if (number == 55) break;
+//        }
+//
+//        System.out.println(Arrays.toString(arrayNumbers));
+//
+//        int [] array2 = {3, 7, 5, 0, 1, 8};
+          int [] array3 = {3, 7, 5, 0, 1, 8};
+          Arrays.sort(array3);
+          System.out.println(Arrays.toString(array3));
+//
+//        int [] array4 = Arrays.copyOfRange(array2, 1, 3);
+//       // array4[3] = 100;
+//        System.out.println(Arrays.toString(array2));
+//        System.out.println(Arrays.toString(array4));
+//
+//        String [] arrayString = new String[array3.length];
+//        int i = 0;
+//        for (int number : array3){
+//            String numberStr = Integer.toString(number);
+//            arrayString[i] = numberStr;
+//            i++;
+//        }
+//        System.out.println(Arrays.toString(array3));
+//        System.out.println(Arrays.toString(arrayString));
+//        int b = Integer.parseInt("45555");
 
     }
 }
